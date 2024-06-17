dd0bdbba92051ad78a772ba820c3ee921acf7e6a
Rishabh Pomaje <97334879+RishP11@users.noreply.github.com>
2024-06-17 17:34:16 +0530

    Add files via upload

--- Source_Codes_simulations/CommSysLib.py
+++ Source_Codes_simulations/CommSysLib.py
@@ -103,9 +103,49 @@ def BPSK_mapper(input_stream, energy_per_symbol) :
 
     return output_stream
 
-#
-# Functions to be used at the receiver
-#
+def AlamoutiEncoder(input_stream, E_b=1):
+    antenna_01_signal = []
+    antenna_02_signal = []
+    idx = 0 
+    s0 = np.sqrt(E_b / 2)
+    s1 = -1 * s0
+    for _ in range(0, len(input_stream), 2):
+        if input_stream[idx] == 0:
+            x1 = s0
+        else:
+            x1 = s1
+        if input_stream[idx+1] == 0:
+            x2 = s0
+        else:
+            x2 = s1
+        antenna_01_signal.append(x1)
+        antenna_02_signal.append(x2)
+        antenna_01_signal.append(-1 * np.conjugate(x2))
+        antenna_02_signal.append(np.conjugate(x1))
+        idx = idx + 2
+    return np.array([antenna_01_signal, antenna_02_signal])
+
+def AlamoutiDecoder(input_stream, csi_01, csi_02, return_bin=True):
+    output_stream = []
+    idx = 0 
+    for m in range(len(csi_01)):
+        y = [input_stream[idx], np.conjugate(input_stream[idx + 1])]
+        c1 = [csi_01[m], np.conjugate(csi_02[m])]
+        c2 = [csi_02[m], -1 * np.conjugate(csi_01[m])]
+        # Estimate 
+        r1 = np.matmul(np.conjugate(c1), y) / np.linalg.norm(c1)
+        r2 = np.matmul(np.conjugate(c2), y) / np.linalg.norm(c2)
+        if return_bin == False:
+            output_stream.append(r1)
+            output_stream.append(r2)
+        else:
+            # BPSK Demapping 
+            output_stream.append(BPSK_demapper(r1))
+            output_stream.append(BPSK_demapper(r2))
+        idx = idx + 2
+
+    return np.array(output_stream).flatten()
+
 def BPSK_demapper(rx_stream) :
     """
     Function to perform Binary Phase Shift Keying (BPSK) constellation demapping.
@@ -148,9 +188,8 @@ def square_law_detector(rx_signal):
     estimated_message = np.where(np.abs(rx_signal_reshaped[:, 0]) > np.abs(rx_signal_reshaped[:, 1]), 0, 1)
 
     return estimated_message
-import numpy as np
 
-def mrc_decoding(input_stream, csi):
+def mrc_decoding(input_stream, csi, return_bin=True):
     """
     Perform Maximal Ratio Combining (MRC) on received complex symbols.
 
@@ -179,10 +218,10 @@ def mrc_decoding(input_stream, csi):
     for i in range(len(signal_01)):
         combined_symbols.append(np.conjugate(np.array([fade_taps_01[i], fade_taps_02[i]])) @ np.array([signal_01[i], signal_02[i]]))
 
-    # Demodulate symbols
-    demodulated_symbols = BPSK_demapper(combined_symbols)
-
-    return demodulated_symbols.flatten()
+    if return_bin == False:
+        return np.array(combined_symbols).flatten()
+    else:
+        return BPSK_demapper(np.array(combined_symbols).flatten())
 
 def hamming_decoder(rx_stream, parity_chk_matrix):
     """
